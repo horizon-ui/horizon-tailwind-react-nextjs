cae73ee0c045592c2b1063d5892ebfdfa5df5ba0
msaif001 <saifmohammed888@gmail.com>
2024-07-28 18:27:12 +0530

    fix: uset setting done

--- src/components/admin/adminUsers/activityTable/[[...index]].tsx
+++ src/components/admin/adminUsers/activityTable/[[...index]].tsx
@@ -97,7 +97,6 @@ const ActivityTable = () => {
                 'rgba(50, 50, 93, 0.25) 0px 30px 60px -12px, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px',
               backgroundColor: 'white', // Ensure background color is set
             }}
-            borderColor="red"
             pagination={{
               pageSize: pageSize,
               showQuickJumper: true,
